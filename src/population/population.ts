5467d34a74ba1e12b6cb14156d546f30904f1a26
Enzo <e.radnai@hotmail.fr>
2020-10-26 12:24:21 +0100

    edit dto

--- src/population/population.ts
+++ src/population/population.ts
@@ -2,7 +2,7 @@
  * Objet contenant la matrice, la taille de la population générale ainsi que le pourcentage d'index à true.
  */
 export class PopulationDto {
-    constructor(public infectedRatio: number = 0, public size: number = 0, public pop: boolean[][][] = [[[false]]]) {
+    constructor(public infectedRatio: number = 0, public size: number = 1, public pop: boolean[][][] = [[[false]]]) {
         this.infectedRatio = infectedRatio
         this.size = size
         this.pop = pop
